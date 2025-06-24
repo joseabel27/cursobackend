7df1f72b1f981a73d22d167575a33142629807fc
Jose Romero <parrajose27@gmail.com>
2025-06-23 20:39:56 -0500

    primer commit

---
+++ clase19_06/tipos_datos.js
@@ -0,0 +1,57 @@
+/* TIPOS DE DATOS USADO EN JS */
+
+/* NUMERICOS */
+const salario = 120000;
+const edad = 40;
+const descuento = 12.5;
+
+/* TEXTO, CADENAS DE CARACTERES ( STRING)*/
+
+const nombre = 'Jose Abel Romero';
+const mensaje = 'hola bienvendido';
+
+/* BOOLEANOS TRUE O FALSE */
+
+const activado = true;
+const desactivado = false;
+
+/*  ARRAYS, ARREGLOS, VECTORES, LISTAS */
+
+const numero = [1,2,3,4,5,6,7];
+
+
+numero.push(8);
+numero.push(9);
+numero.push(10); 
+
+// console.log(numero);
+
+// NO SE SABE CUANTAS POSICIONES TIENE EL ARREGLO
+
+const numeros = [10,20,30,40,50,60,70,80,90,100];
+
+// for (i=0;i<numeros.length; i++){
+
+    // console.log(numeros[i])
+
+
+// OPTIMIZAR EL FOR SIN TENER UN INDEX (i=0)
+
+//  for (const item of numeros){
+
+    // console.log(item);
+
+ 
+
+//  OTRA FORMA DE RECORRE EL ARREGLO MAS OPTIMIZADO
+
+// numeros.map((item)=>console.log(item));
+// numeros.forEach(item=>console.log(item));
+
+let i = 0;
+
+while (i<numeros.length){
+
+    console.log(numeros[i]);
+    i++; 
+}
---
+++ clase19_06/variables.js
@@ -0,0 +1,9 @@
+git /* las variables en JavaScript
+Var, Const, Let */
+
+/* const edad = 40;
+edad = 41; */
+
+let edad = 40;
+console.log('Usted tiene', edad,'años');
+edad = 41;
