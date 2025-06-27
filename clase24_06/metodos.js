b6a4f6676eae98c30c1d7547975531f5cda0c1a6
Jose Romero <parrajose27@hotmail.com>
2025-06-27 13:13:37 -0500

    agregand

---
+++ clase24_06/metodos.js
@@ -0,0 +1,42 @@
+/* METODOS QUE MAS SE UTILIZAN CON ARREGLOS */
+
+/* 1) PUSH() : AGREGA UNO O MAS ELEMENTOS AL FINAL DEL ARREGLO */
+// RETORNA : LA NUEVA LONGITUD DEL ARREGLO
+
+// EJEMPLO :
+
+// const arr1 = [1,2,3];
+// arr1.push(4);
+
+// console.log(arr1);
+
+/* 2) POP() : ELIMINA EL ULTIMO ELEMENTO DEL ARREGLO */
+// RETORNA : EL ELEMENTO ELIMINADO.
+
+// EJEMPLO:
+
+// const arr2 = [1,2,3,4,5,6];
+// arr2.pop();
+
+// console.log(arr2);
+
+/* 3) SHIFT() : ELIMINA EL PRIMER ELEMENTO DEL ARREGLO */
+// RETORNA:EL ELEMENTO ELIMINADO
+
+// EJEMPLO:
+
+// const arr3 =[4,3,2,4,5];
+// arr3.shift();
+
+// console.log(arr3);
+
+
+/* 4) UNSHIFT() : AGREGA UNO O MAS ELEMENTOS AL PRINCIPIO DEL ARREGLO */
+// RETORNA: LA NUEVA LONGITUD DEL ARREGLO.
+
+// EJEMPLO:
+
+const arr4 =[3,2,4,6];
+arr4.unshift(1,3,4);
+
+console.log(arr4);
