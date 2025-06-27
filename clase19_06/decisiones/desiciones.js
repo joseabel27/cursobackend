ac5be47ec32b9ffeab2ddf4dc0ccadabc7011c7a
Jose Romero <parrajose27@gmail.com>
2025-06-26 21:09:15 -0500

    agregando ejercicios

---
+++ clase19_06/decisiones/desiciones.js
@@ -0,0 +1,146 @@
+/* DESICIONES */
+
+
+
+// OPERADOR == (IGUALDA DEBIL)
+
+// const numero = 5;
+// const textnumero="5";
+
+// if(numero==textnumero){
+
+//     console.log("Son Iguales");
+// }else {
+
+//     console.log("No son iguales");
+// }
+
+
+
+// OPERADOR === (IGUALDA ESTRICTA)
+
+
+// const numero = 5;
+// const textnumero="5";
+
+// if(numero===textnumero){
+
+//     console.log("Son Iguales");
+// }else {
+
+//     console.log("No son iguales");
+// }
+
+
+// OPERADOR !== (DESIGUALDA ESTRICTA)
+
+//  const numero = 10;
+//  const numerotext = "10";
+
+//  if (numero !== numerotext){
+
+//     console.log("No es igual");
+//  }else {
+
+//     console.log("Son Iguales");
+//  }
+
+ /* OPERADOR MAYOR QUE */
+
+// const numeroA = 50;
+// const numeroB = 40;
+
+// if (numeroA > numeroB){
+
+//     console.log("a es mayor");
+// }else {
+
+//     console.log("b es mayor");
+// }
+
+/*  OPERADOR MENOR QUE */
+
+// const edad = 26 ;
+
+// if ( edad < 25){
+
+//     console.log("No puedes manejar aviones de combate");
+// }else {
+
+//     console.log("Si puedes manejar aviones de combate");
+// }
+
+/* OPERADOR MAYOR O IGUAL QUE */
+
+// const edad = 19;
+
+// if (edad >=20){
+
+//     console.log("Puedes entrar a la disco");
+// }else {
+
+//     console.log("No puedes entrar a la disco");
+// }
+
+/* OPERADOR MENOR O IGUAL QUE */
+
+// const talla = 42;
+
+// if (talla <= 40){
+
+//     console.log("No eres paton");
+// }else {
+
+//     console.log("Eres paton");
+// }
+
+
+/* OPERADOR && */
+
+// const edad =20;
+// const hombre = true;
+
+// if( edad >= 20 && hombre){
+
+//     console.log("Puede ingresar al spa de hombres");
+// }else {
+
+//     console.log("No puedes igresar al spa");
+// }
+
+/* OPERADOR OR */
+
+// const perro = false;
+// const gato = false;
+
+// if( perro || gato){
+
+//     console.log("Puedes comer perrarina");
+// }else {
+
+//     console.log("Eres humano, come comida");
+// }
+
+/* OPERADOR ! NOT */
+
+// 
+
+/* OPERADOR COMBINADOS && || */
+
+// const descuentoFibo=true;
+// const imbalance=false;
+// const hayOb=false;
+
+// if ( descuentoFibo && (imbalance || hayOb)){
+
+//     console.log("Puedes tomar la operacion");
+// }else {
+
+//     console.log("No debes tomar la entrada");
+// }
+
+/* OPERADOR TERNARIO */
+
+const edad =17;
+
+edad >=18?console.log("Puedes beber"):console.log("No puedes beber");
--- clase19_06/funciones/funciones.js
+++ clase19_06/funciones/funciones.js
@@ -10,12 +10,73 @@
 
 /* FUNCION CON PARAMETROS */
 
-function saludar(nombre){
+// function saludar(item){
 
-    console.log(
-}
+//     console.log(`hola, ${item}`);
+// }
 
-const nombre="jose";
-saludar(nombre);
+// const nombre="jose";
+// saludar(nombre);
+
+/* FUNCION QUE RETORNE UN VALOR */
+
+// function sumar(n1,n2){
+
+//      const resultado = n1+n2;
+//      return resultado;
+// }
+
+// const result = sumar(5,10);
+// console.log(result);
+
+/* FUNCIONES FLECHAS */
+
+// const saludar = () => {
+
+//     console.log("Hola Amigo");
+// };
+
+// saludar();
 
 
+// const saludar = (item)=>{
+
+//      console.log(`hola, ${item}`);
+//  }
+
+//  const nombre="jose";
+// saludar(nombre);
+
+// const sumar=(n1,n2)=>{
+
+//  const resultado = n1+n2;
+//       return resultado;
+//  }
+
+//  const result = sumar(5,10);
+//  console.log(result);
+
+/* FUNCIONES FLECHAS CON RETORNO IMPLICITO */
+
+// const sumar = (n1,n2) => n1+n2;
+// const result = sumar (5,10);
+// console.log(result);
+
+
+/*  FUNCIONES FLECHAS CON OBJETOS Y DESESTRUCTURACION*/
+
+const saludar3 =(persona)=>{
+
+    const {nombre,apellido,ciudad}=persona;
+
+    console.log(`hola ${nombre} ${apellido} bienvenido, veo que eres de la ciudad de ${ciudad}`);
+};
+
+const persona ={
+
+    nombre:"Jose",
+    apellido:"Romero",
+    ciudad:"Medellin",
+};
+
+saludar3(persona);
